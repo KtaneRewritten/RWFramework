5aebabce8cddcbcbf64e0d786d76a9fc40404b7d
UnrulyJuli3 <sid3rdark@gmail.com>
2024-09-25 13:39:12 -0700

    Add RWBombModule

---
+++ RWBombModule.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Used for additional options in conjunction with <see cref="KMBombModule"/>.
+/// </summary>
+[RequireComponent(typeof(KMBombModule))]
+public class RWBombModule : MonoBehaviour
+{
+    /// <summary>
+    /// Whether the module requires a deeper foam backing in the bomb casing.
+    /// </summary>
+    [Tooltip("Whether the module requires a deeper foam backing in the bomb casing.")]
+    public bool RequiresDeepBacking;
+}
