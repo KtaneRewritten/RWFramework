b9abd391d05ab7dc5e486efb7b1bd89f9f4515af
UnrulyJuli3 <sid3rdark@gmail.com>
2024-09-25 14:18:21 -0700

    Add RWGameInfo

---
+++ RWGameInfo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Defines information about the active game, like mission info. Can be used without any extra components.
+/// </summary>
+public class RWGameInfo : MonoBehaviour
+{
+    /// <summary>
+    /// Is this mod running in Rewritten?
+    /// </summary>
+    public bool IsRewritten { get; set; } = false;
+
+    /// <summary>
+    /// The information for the current mission. May be null.
+    /// </summary>
+    public KMMission Mission => OnGetMission?.Invoke() ?? null;
+
+    /// <summary>
+    /// Do not use in mod. For game use only.
+    /// </summary>
+    public event GetMissionDelegate OnGetMission;
+
+#pragma warning disable CS1591
+    public delegate KMMission GetMissionDelegate();
+#pragma warning restore CS1591
+}
