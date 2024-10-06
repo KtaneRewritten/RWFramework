9b88ec8551a4539e12c993b71452df4b66b8e885
UnrulyJuli3 <sid3rdark@gmail.com>
2024-10-06 12:52:36 -0700

    add RWGameCommands

---
+++ RWGameCommands.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Provides methods to interact with the game.
+/// </summary>
+public class RWGameCommands : MonoBehaviour
+{
+    /// <summary>
+    /// Whether or not the room lights are on. Can be set to turn the lights on or off.
+    /// </summary>
+    public bool RoomLightsOn
+    {
+        get => OnGetRoomLights?.Invoke() ?? false;
+        set => OnSetRoomLights?.Invoke(value);
+    }
+
+    /// <summary>
+    /// Do not use in mod. For game use only.
+    /// </summary>
+    public event GetRoomLightsDelegate OnGetRoomLights;
+
+    /// <summary>
+    /// Do not use in mod. For game use only.
+    /// </summary>
+    public event SetRoomLightsDelegate OnSetRoomLights;
+
+#pragma warning disable CS1591
+    public delegate bool GetRoomLightsDelegate();
+    public delegate void SetRoomLightsDelegate(bool on);
+#pragma warning restore CS1591
+}
